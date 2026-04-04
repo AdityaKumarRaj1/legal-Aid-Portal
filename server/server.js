58afd2e5ee200db4a256678806986438100f3b87
Aditya Kumar Raj <adityakraj19@gmail.com>
2026-04-04 22:35:43 +0530

    fix cors for vercel

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: function (origin, callback) { callback(null, true); }, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
